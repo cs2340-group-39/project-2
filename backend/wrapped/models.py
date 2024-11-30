d2c403d401951df033592f8bab9f910f23b8007c
Arjun Sheshadri <64653918+GELIOS-19@users.noreply.github.com>
2024-11-30 14:59:31 -0500

    Commit

--- backend/wrapped/models.py
+++ backend/wrapped/models.py
@@ -1,2 +1,24 @@
 from django.db import models
 
+from ..users.models import Profile
+
+
+class WrappedManager(models.Manager):
+  def create(self):
+    wrapped = self.model()
+    wrapped.save()
+    return wrapped
+
+  def get():
+    """Filter by profile."""
+    super.create()
+
+
+class Wrapped(models.Model):
+  profile = models.ForeignKey(
+    Profile,
+    on_delete=models.CASCADE,
+    related_name="wrapped_for_profile",
+  )
+
+  objects = WrappedManager()
