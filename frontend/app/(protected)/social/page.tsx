6c1ebba2fc16125100458cf6b15c335254c47a54
Saad Ata <saad@mac.lan>
2024-11-30 13:58:49 -0600

    added tabs for social

--- frontend/app/(protected)/social/page.tsx
+++ frontend/app/(protected)/social/page.tsx
@@ -1,5 +1,6 @@
 import { WrappedData } from "../actions";
 import { SocialWrappedSection } from "./social-wrapped-section";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export default function Page() {
   const carouselsData: WrappedData[] = [
@@ -11,5 +12,22 @@ export default function Page() {
     },
   ];
 
-  return <SocialWrappedSection carouselsData={carouselsData} />;
+  return (
+    <div className="flex flex-col items-center">
+    <Tabs defaultValue="all" className="w-full max-w-full">
+      <TabsList className="flex justify-center">
+        <TabsTrigger value="all">All</TabsTrigger>
+        <TabsTrigger value="liked">Liked</TabsTrigger>
+        <TabsTrigger value="following">Following</TabsTrigger>
+      </TabsList>
+      <TabsContent value="all">
+        <div className="w-full">
+          <SocialWrappedSection carouselsData={carouselsData} />
+        </div>
+      </TabsContent>
+      <TabsContent value="liked">Show liked here.</TabsContent>
+      <TabsContent value="following">Show following here.</TabsContent>
+    </Tabs>
+    </div>
+  )
 }
